4706d78422a9ba45edabbb676033a1db440c194b
kl2ag1 <julian.gajek@lo1.sandomierz.pl>
2019-10-30 09:42:44 +0100

    jezynt

--- inc/functions.php
+++ inc/functions.php
@@ -1,38 +1,43 @@
 <?php
 
-//$pages = array(
-//	'witam' => 'Witamy',
-//	'formularz' => 'Formularz',
-//	'klasa' => 'Klasy'
-//);
+// $pages = array(
+// 	'witam' => 'Witamy',
+// 	'formularz' => 'Formularz',
+// 	'klasa' => 'Klasy'
+// );
+
+//github.com/lo1cgsan/phpapp
+
+$ret = array();
 
 function get_menu($id) {
 	global $db;
 	$ret = array();
 	db_query('SELECT * FROM menu', $ret);
-	print_r($ret);
-  foreach ($ret as $k => $t) {
+	//print_r($ret);
+ 	foreach ($ret as $k => $t) {
 		echo '
 <li class="nav-item">
-    <a class="nav-link" href="?id='.$t['plik'].'">'.$t[].'</a>
+    <a class="nav-link" href="?id='.$t['plik'].'">'.$t['tytul'].'</a>
 </li>
 		';
 	}
 }
-
 function get_page_title($id) {
-	global $pages;
-	if (array_key_exists($id, $pages))
-		echo $pages[$id];
-	else
-		echo 'Aplikacja PHP';
+	global $ret;
+  foreach ($ret as $k => $t) {
+		if ($t['plik'] == $id) {
+			echo $t['tytul'];
+			return;
+		}
+	}
+	//tutuł domyślny
+	echo 'Aplikacja PHP';
 }
-
 function get_page_content($id) {
 	if (file_exists($id.'.html'))
 		include($id.'.html');
 	else
 		include('404.html');
 }
-
 ?>
