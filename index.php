da2c917781931ed56f37818eb5c28d21f4df9e53
kl2ag1 <julian.gajek@lo1.sandomierz.pl>
2019-11-20 12:46:43 +0100

    nieeee

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('log_errors', 1);
 ini_set('error_log', 'errorlog.txt');
-
 define('DINC', 'inc/');
 define('DBASE', 'db/');
 $dbfile = DBASE.'baza.db';
@@ -14,18 +13,15 @@ $kom = array();
 require_once(DINC.'functions.php');
 require_once(DINC.'db.php');
 require_once(DINC.'users.php');
-
 init_baza();
 init_tables();
-
+$user = new User();
 if (isset($_GET['id']))
 	$id=$_GET['id'];
 else
 	$id='witam';
-
 include_once(DINC.'template.php');
-
-echo 'zaq1@WSX';
-echo sha1('zaq1@WSX');
-
+// echo 'zaq1@WSX';
+// echo sha1('zaq1@WSX');
+// c380f833034d60bf035a134094eb538d600dc6f9
 ?>
